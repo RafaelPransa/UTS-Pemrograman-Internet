b797b8e42fceac24b5ec419e1d98ed93835713bd
Rafael Pransa <44833804+RafaelPransa@users.noreply.github.com>
2025-10-03 02:08:10 +0700

    Membuat responsive bagian nav main-hero dan mobile-nav

---
+++ script/app.js
@@ -0,0 +1,22 @@
+const header = document.getElementsByTagName('header');
+const nav = document.querySelector('header nav');
+const startNav = document.querySelector('header nav .start-nav');
+const midNav = document.querySelector('header nav .mid-nav');
+const endNav = document.querySelector('header nav .end-nav');
+const mobileNav = document.querySelector('main .mobile-nav');
+
+console.log('adad');
+
+// Memindahkan ke parent yang lain
+function moveNavElements () {
+    if (window.innerWidth < 768) {
+        midNav.style.display = 'none';
+        endNav.style.display = 'none';
+    } else {
+        midNav.style.display = 'block';
+        endNav.style.display = 'block';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', moveNavElements);
+window.addEventListener('resize', moveNavElements);
