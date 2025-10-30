eed817be4ab5718b49e73f27429f451e398614e8
Rafael Pransa <44833804+RafaelPransa@users.noreply.github.com>
2025-10-30 17:14:29 +0700

    tambah dan hapus transaksi

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
-    const header = document.querySelector('.header-hero-content-wrapper header');
     const nav = document.querySelector('.header-hero-content-wrapper header nav');
     const wrapperHeader = document.querySelector('.header-hero-content-wrapper');
     const menuNavA = document.querySelectorAll('.header-hero-content-wrapper header nav .mid-nav ul li a');
+    const hero = document.querySelector('.header-hero-content-wrapper #hero');
     window.addEventListener('scroll', function() {
         if (window.scrollY > 10) {
-            if (window.innerWidth > 430) {
-                header.classList.add('nav-transparent');
-                header.classList.add('nav-scroll');
-                menuNavA.forEach(menus => {
-                    menus.classList.add('color-a-scroll');
-                });
-            }
+            nav.classList.add('nav-scroll');
+            hero.classList.add('hero-scroll');
+            menuNavA.forEach(menus => {
+                menus.classList.add('color-a-scroll');
+            });
         } else {
-            header.classList.remove('nav-scroll');
+            nav.classList.remove('nav-scroll');
+            hero.classList.remove('hero-scroll');
             menuNavA.forEach(menus => {
                     menus.classList.remove('color-a-scroll');
                 });
@@ -24,37 +22,11 @@ document.addEventListener('DOMContentLoaded', () => {
     function checkNavStatus() {
         const isNavScrolled = nav.classList.contains('nav-scroll');
         if (isNavScrolled) {
-            if (window.innerWidth > 430) {
-                wrapperHeader.style.paddingTop = '85px';
-            }
+            wrapperHeader.style.paddingTop = '100px';
         } else {
             wrapperHeader.style.paddingTop = '20px';
         }
     }
     window.addEventListener('scroll', checkNavStatus);
     checkNavStatus();
-
-    // GSAP
-    gsap.fromTo('body header', {
-        y: -200, opacity: 0
-    }, { 
-        y: 0, opacity: 1, duration: 1
-    });
-    
-    gsap.fromTo('body .header-hero-content-wrapper #hero', {
-        y: 200, opacity: 0
-    }, {
-        y: 0, opacity: 1, duration: 1
-    });
-
-    gsap.fromTo('body .header-hero-content-wrapper #hero .hero-content', {
-        x: -200, opacity: 0
-    }, {
-        x: 0, opacity: 1, delay: 1
-    });
-    gsap.fromTo('body .header-hero-content-wrapper #hero .hero-illustration', {
-        x: 200, opacity: 0
-    }, {
-        x: 0, opacity: 1, delay: 1
-    });
 });
--- scripts/catatan.js
+++ scripts/catatan.js
@@ -16,4 +16,117 @@ document.addEventListener('DOMContentLoaded', () => {
 
     window.addEventListener('resize', repositionButton);
     repositionButton(); // Jalankan sekali saat dimuat
+
+    // pop up form tambah transaksi
+    const modal = document.getElementById('transaction-modal');
+    const openBtn = document.querySelector('#action-bar button');
+    const closeBtn = document.getElementById('close-modal-btn');
+    const form = document.getElementById('transaction-form');
+
+    openBtn.addEventListener('click', () => {
+        document.getElementById('date').value = new Date().toISOString().substring(0, 10);
+        modal.classList.remove('modal-hidden');
+    });
+
+    closeBtn.addEventListener('click', () => {
+        modal.classList.add('modal-hidden');
+        form.reset();
+    });
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            modal.classList.add('modal-hidden');
+            form.reset();
+        };
+    });
+
+    // Simpan data(create)
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const newRecord = {
+        id: Date.now(),
+        type: form.type.value,
+        item: form.item.value,
+        amount: parseFloat(form.amount.value),
+        date: form.date.value
+        };
+
+        saveRecord(newRecord);
+        modal.classList.add('modal-hidden');
+        form.reset();
+
+        loadTransactions();
+    });
+
+    // Local Storage
+    function getRecords() {
+        const recordsJSON = localStorage.getItem('financeRecords');
+        return recordsJSON ? JSON.parse(recordsJSON) : [];
+    };
+
+    function saveRecord(record) {
+        let records = getRecords();
+        records.push(record);
+        localStorage.setItem('financeRecords', JSON.stringify(records));
+    };
+
+    function loadTransactions() {
+        const records = getRecords();
+        const container = document.querySelector('.container-list-transactions');
+
+        let html = '<h2>Daftar Transaksi</h2>';
+
+        if (records.length === 0) {
+            html += '<p>Belum ada transaksi yang tercatat.</p>';
+        } else {
+            records.forEach(record => {
+                const sign = record.type === 'income' ? '+' : '-';
+                const amountClass = record.type;
+
+                html += `
+                <div class="transaction">
+                    <div class="illustration"></div>
+                    <div class="explanation">
+                        <h3>${record.item}</h3>
+                        <p>${new Date(record.date).toLocaleDateString()}</p>
+                    </div>
+                    <div class="interaction">
+                        <h3 class="${amountClass}">${sign} Rp ${record.amount.toLocaleString('id-ID')}</h3>
+                        <div class="choise">
+                            <button type="button" class="detail" data-id="${record.id}">Detail</button>
+                            <button type="button" class="edit" data-id="${record.id}">Edit</button>
+                            <button type="button" class="delete" data-id="${record.id}">Hapus</button>
+                        </div>
+                    </div>
+                </div>
+                `;
+            });
+        };
+        container.innerHTML = html;
+        setupTransactionListeners();
+    };
+
+    // Delete data
+    function deleteRecord(id) {
+        let records = getRecords();
+
+        records = records.filter(record => record.id !== Number(id));
+
+        localStorage.setItem('financeRecords', JSON.stringify(records));
+    };
+
+    function setupTransactionListeners() {
+        const deleteBtn = document.querySelectorAll('.delete');
+        deleteBtn.forEach(button => {
+            button.addEventListener('click', function () {
+                const recordId = this.getAttribute('data-id');
+                if (confirm(`Apakah anda yakin ingin menghapus transaksi (ID: ${recordId})`)) {
+                    deleteRecord(recordId);
+                    loadTransactions();
+                };
+            });
+        });
+    };
+    loadTransactions();
 });
